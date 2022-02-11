a35ab21a3052070f76d3185cb0911d9f97615752
Ben Halverson <benhalverson33@gmail.com>
2022-02-11 11:34:58 -0800

    styling with tailwind

--- components/image.tsx
+++ components/image.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 
-const MyImage = ({ src, alt, width, height }: Props) => {
+const MyImage = ({ src, alt }: Props) => {
   return (
-    <>
-      <Image src={src} alt={alt} width="1000" height="1000" />
-    </>
+    <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+      <Image
+        src={src}
+        alt={alt}
+        width="1000"
+        height="1000"
+        className="object-cover pointer-events-none group-hover:opacity-75"
+      />
+    </div>
   );
 };
 export default MyImage;
--- components/imageList.tsx
+++ components/imageList.tsx
@@ -3,17 +3,25 @@ import MyImage from "./image";
 
 const ImageList = ({ images }: any) => {
   return (
-    <div>
-      <h1>Image List</h1>
-      <ul>
+    <div className="flex flex-center">
+      <ul
+        role="list"
+        className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+      >
         {images.map((image: any) => (
-          <li key={image.id}>
+          <li key={image.id} className="relative">
             <MyImage
               src={image.urls.small}
               alt={image.description}
               width={image.width}
               height={image.height}
             />
+            <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
+              <a href={image.links.html} target="_blank" rel="noopener noreferrer">Link</a>
+            </p>
+            <p className="block text-sm font-medium text-gray-500 pointer-events-none">
+              {image.description}
+            </p>
           </li>
         ))}
       </ul>
