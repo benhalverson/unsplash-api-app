6191ba42fa29f71754fa9ffe4618ddcdda41576d
Ben Halverson <benhalverson33@gmail.com>
2022-02-11 19:23:16 -0800

    created function to use mockdata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import ImageList from "../components/imageList";
 import Header from "../components/header";
@@ -7,7 +7,7 @@ import Header from "../components/header";
 const Home: NextPage = () => {
   const [images, setImages] = useState<string[]>([]);
   const [search, setSearch] = useState("");
-  const getPhotos = async () => {
+  const getPhotos = async (): Promise<[]> => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       headers: {
         Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
@@ -23,9 +23,21 @@ const Home: NextPage = () => {
     return photos;
   };
 
-  useEffect(() => {
-    getPhotos();
-  }, []);
+  const getMockedPhotos = async () => {
+    const response = await axios.get("/api/unsplash", {
+      headers: {
+        Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+      },
+      params: {
+        query: `${search}`,
+      },
+    });
+    const photos = response.data.results;
+    if (photos) {
+      setImages(photos);
+    }
+    return photos;
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -33,7 +45,8 @@ const Home: NextPage = () => {
 
   const handleSubmit = (e: any) => {
     setSearch(e.target.value);
-    getPhotos();
+    // getPhotos();
+    getMockedPhotos();
   };
 
   return (
@@ -73,7 +86,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <ImageList images={images} />
+      <ImageList images={images}  />
     </div>
   );
 };
